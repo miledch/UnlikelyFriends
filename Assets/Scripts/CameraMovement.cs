c996380d1f5a668f38aebf423fdb26aacb0d7f4c
Matthew Kevork <matthew.kevork@hotmail.com>
2018-05-19 14:18:42 -0400

    Side scrolling camera feature

---
+++ Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour {
+
+    private Rigidbody2D rigidbody2D;
+    public float speed = 0.25f;
+    public float acceleration = 0.01f;
+    float updateVelocity;
+
+    // Use this for initialization
+    void Start () {
+        rigidbody2D = GetComponent<Rigidbody2D>();
+
+        this.rigidbody2D.velocity += Vector2.right * speed * Time.deltaTime;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        updateVelocity = this.rigidbody2D.velocity.x * acceleration;
+
+        this.rigidbody2D.velocity += Vector2.right * acceleration * Time.deltaTime;
+
+
+    }
+
+
+}
---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelManager : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void LoadLevel(string name)
+    {
+        Debug.Log("Level load requested for: " + name);
+        SceneManager.LoadScene(name);
+
+
+    }
+
+    public void QuitRequest()
+    {
+        Debug.Log("Quit requested");
+
+        Application.Quit();
+    }
+
+    public void LoadNextLevel()
+    {
+        print(SceneManager.GetActiveScene().buildIndex + 1);
+
+
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void LoadLastLevel()
+    {
+        print(SceneManager.sceneCount - 1);
+        SceneManager.LoadScene(SceneManager.sceneCount - 1);
+    }
+
+
+    /*
+    public void BrickDestroyed()
+    {
+        if (Brick.breakableCount == 0)
+        {
+            buildIndex++;
+
+            print("index = " + buildIndex);
+            print("winIndex = " + winIndex);
+
+            if (buildIndex == winIndex)
+            {
+                buildIndex = 2;
+            }
+            else
+            {
+
+            }
+            LoadNextLevel();
+        }
+    }
+
+    */
+
+}
