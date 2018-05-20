543237ed5cb45b7ba3c68898f1db9524dee7e45f
Matthew Kevork <matthew.kevork@hotmail.com>
2018-05-20 11:59:14 -0400

    Start music added to menu

---
+++ Assets/Scripts/MusicPlayer.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicPlayer : MonoBehaviour {
+
+    static MusicPlayer instance = null;
+
+    void Awake()
+    {
+        Debug.Log("Music player Awake " + GetInstanceID());
+
+        if (instance != null)
+        {
+            Destroy(gameObject);
+            print("duplicate music player self destructed");
+        }
+        else
+        {
+            instance = this;
+            GameObject.DontDestroyOnLoad(gameObject);
+        }
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+        Debug.Log("Music player Start " + GetInstanceID());
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
